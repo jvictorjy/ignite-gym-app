d6467c8a33441c4ffaa4e35f71a7333129c01b37
Victor Ribeiro <jvictorjy@gmail.com>
2024-07-18 01:42:47 -0300

    feat: create screen signin

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { Loading } from '@components/Loading';
 import { THEME } from 'src/theme';
+import { Signin } from '@screens/Signin';
 
 export default function App() {
   const [fonstsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -15,7 +16,7 @@ export default function App() {
         translucent
       />
 
-      {fonstsLoaded ? <View /> : <Loading />}
+      {fonstsLoaded ? <Signin /> : <Loading />}
     </NativeBaseProvider>
   );
 }
---
+++ src/components/Input.tsx
@@ -0,0 +1,23 @@
+import { IInputProps, Input as InputNativeBase } from 'native-base'
+
+export function Input({ ...rest }: IInputProps) {
+    return (
+        <InputNativeBase  
+            bg="gray.700"
+            h={14}
+            px={4}
+            borderWidth={0}
+            fontSize="md"
+            color="white"
+            fontFamily="bold"
+            mb={4}
+            placeholderTextColor="gray.300"
+            _focus={{
+                bg: "gray.700",
+                borderWidth: 1,
+                borderColor: "green.500"
+            }}
+            {...rest}
+        />
+    )
+}
